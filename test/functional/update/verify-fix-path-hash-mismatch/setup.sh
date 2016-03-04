81eed9b75e82ca0e6099eba438450f556b845ed0
Archana Shinde <archana.m.shinde@intel.com>
2016-03-04 08:29:31 -0800

    Add functional tests for testing verify_fix_path functionality.
    The tests check for missing directory and hash mismatch for a parent directory.

---
+++ test/functional/update/verify-fix-path-hash-mismatch/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+rm -f web-dir/*/*.tar
+tar -C web-dir/10  -cf web-dir/10/Manifest.MoM.tar Manifest.MoM Manifest.MoM.signed
+tar -C web-dir/100  -cf web-dir/100/Manifest.MoM.tar Manifest.MoM Manifest.MoM.signed
+tar -C web-dir/10  -cf web-dir/10/Manifest.os-core.tar Manifest.os-core Manifest.os-core.signed
+tar -C web-dir/10 -cf web-dir/10/Manifest.test-bundle.tar Manifest.test-bundle Manifest.test-bundle.signed
+tar -C web-dir/100 -cf web-dir/100/Manifest.test-bundle.tar Manifest.test-bundle Manifest.test-bundle.signed
+sudo chown root:root web-dir/10/files/24d8955d9952c3fcb2241b0f8d225205a5861cec9757b3a075d34810da9b08af
+sudo chown root:root web-dir/10/files/cde33514c151abb2b01448be290ab1d5212952571d15e3533cadc15ff82f2cd5
+sudo chown root:root web-dir/100/files/c2fb5e545287e632581b82e371de5d4aac2c7eb50d7f357f3b66fb0fdca71dd9
+tar -C web-dir/100/files -cf web-dir/100/files/c2fb5e545287e632581b82e371de5d4aac2c7eb50d7f357f3b66fb0fdca71dd9.tar c2fb5e545287e632581b82e371de5d4aac2c7eb50d7f357f3b66fb0fdca71dd9
+tar -C web-dir/10/files -cf web-dir/10/files/24d8955d9952c3fcb2241b0f8d225205a5861cec9757b3a075d34810da9b08af.tar 24d8955d9952c3fcb2241b0f8d225205a5861cec9757b3a075d34810da9b08af --exclude=24d8955d9952c3fcb2241b0f8d225205a5861cec9757b3a075d34810da9b08af/*
+tar -C web-dir/10/files -cf web-dir/10/files/cde33514c151abb2b01448be290ab1d5212952571d15e3533cadc15ff82f2cd5.tar cde33514c151abb2b01448be290ab1d5212952571d15e3533cadc15ff82f2cd5 --exclude=cde33514c151abb2b01448be290ab1d5212952571d15e3533cadc15ff82f2cd5/*
---
+++ test/functional/update/verify-fix-path-hash-mismatch/teardown.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+sudo chown $(ls -l setup.sh | awk '{ print $3 ":" $4 }') web-dir/10/files/24d8955d9952c3fcb2241b0f8d225205a5861cec9757b3a075d34810da9b08af
+sudo chown $(ls -l setup.sh | awk '{ print $3 ":" $4 }') web-dir/10/files/cde33514c151abb2b01448be290ab1d5212952571d15e3533cadc15ff82f2cd5
+sudo chown $(ls -l setup.sh | awk '{ print $3 ":" $4 }') web-dir/100/files/c2fb5e545287e632581b82e371de5d4aac2c7eb50d7f357f3b66fb0fdca71dd9
+sudo chown $(ls -l setup.sh | awk '{ print $3 ":" $4 }') target-dir/usr 
+sudo rm -rf target-dir/usr/bin/foo
+sudo rmdir target-dir/usr/bin

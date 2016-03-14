43928a1cb72dacc92b32884023693f9bbed7cf41
Brad T. Peters <brad.t.peters@intel.com>
2016-03-14 16:55:40 -0700

    swupd-client: Functional test for the Search
    This patch adds a host of new functional tests, ensuring the search
    feature works as expected.
    
    Signed-off-by: Brad T. Peters <brad.t.peters@intel.com>

---
+++ test/functional/search/content-check/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+rm -f web-dir/*/*.tar
+touch target-dir/test-file
+touch target-dir/usr/share/clear/bundles/test-bundle
+tar -C web-dir/10  -cf web-dir/10/Manifest.MoM.tar Manifest.MoM Manifest.MoM.signed
+tar -C web-dir/10 -cf web-dir/10/Manifest.os-core.tar Manifest.os-core Manifest.os-core.signed
+tar -C web-dir/10 -cf web-dir/10/Manifest.test-bundle.tar Manifest.test-bundle Manifest.test-bundle.signed

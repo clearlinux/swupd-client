2520bfaed72e9d8cc38dcbdc1f7ca15b216e2591
Brad T. Peters <brad.t.peters@intel.com>
2016-03-17 10:50:24 -0700

    v3 of search testing
    Signed-off-by: Brad T. Peters <brad.t.peters@intel.com>

--- test/functional/search/content-check-negwrongpath/setup.sh
+++ test/functional/search/content-check-negwrongpath/setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-touch target-dir/usr/share/clear/bundles/test-bundle
+rm -f web-dir/*/*.tar
 tar -C web-dir/10  -cf web-dir/10/Manifest.MoM.tar Manifest.MoM Manifest.MoM.signed
 tar -C web-dir/10 -cf web-dir/10/Manifest.os-core.tar Manifest.os-core Manifest.os-core.signed
 tar -C web-dir/10 -cf web-dir/10/Manifest.test-bundle.tar Manifest.test-bundle Manifest.test-bundle.signed

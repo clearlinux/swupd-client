8dc4cb8e1858f35f8ca59e66b62dc96b717937d3
Patrick McCarty <patrick.mccarty@intel.com>
2016-04-04 22:33:41 -0700

    Fix a bundle-remove test that failed to load manifests
    The Manifest.os-core was present, but the compressed version was not.
    Fix the issue by providing the compressed version.
    
    Signed-off-by: Patrick McCarty <patrick.mccarty@intel.com>

--- test/functional/bundleremove/boot-file/setup.sh
+++ test/functional/bundleremove/boot-file/setup.sh
@@ -3,6 +3,7 @@
 set -e
 
 tar -C web-dir/10 -cf web-dir/10/Manifest.MoM.tar Manifest.MoM Manifest.MoM.signed
+tar -C web-dir/10 -cf web-dir/10/Manifest.os-core.tar Manifest.os-core Manifest.os-core.signed
 tar -C web-dir/10 -cf web-dir/10/Manifest.test-bundle.tar Manifest.test-bundle Manifest.test-bundle.signed
 
 sudo chown root:root target-dir/usr/lib/kernel/testfile

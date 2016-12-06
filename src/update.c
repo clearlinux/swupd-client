dc003cd9c5760264e46160103ab04337b717d534
Auke Kok <auke-jan.h.kok@intel.com>
2016-12-06 10:04:03 -0800

    Change telemetry API - add class component.
    We change the telemetry API to pass a class record identifier,
    which will be part of the telemetry record file name. This allows
    us easier to send different telemetry class records to the server,
    which will make parsing simpler in the probe and on the backend.
    
    Rev record version to #2 because of this change.

--- src/update.c
+++ src/update.c
@@ -407,7 +407,7 @@ clean_curl:
 	clock_gettime(CLOCK_MONOTONIC_RAW, &ts_stop);
 	delta = ts_stop.tv_sec - ts_start.tv_sec + ts_stop.tv_nsec / 1000000000.0 - ts_start.tv_nsec / 1000000000.0;
 	telemetry(ret ? TELEMETRY_CRIT : TELEMETRY_INFO,
-		"action=update-completed\n"
+		"update",
 		"current_version=%d\n"
 		"server_version=%d\n"
 		"result=%d\n"

09c4b2cfa715a90bc42e7a0498760bc05c57cf82
Icarus Sparry <icarus.w.sparry@intel.com>
2017-04-28 11:33:19 -0700

    Add swupd autoupdate command
    "swupd autoupdate" returns 0 if autoupdates are enabled.
    "sudo autoupdate --enable" will unmask swupd-update.service
    "sudo autoupdate --disable" will mask swupd-update.service
    
    Signed-off-by: Icarus Sparry <icarus.w.sparry@intel.com>

--- include/swupd-internal.h
+++ include/swupd-internal.h
@@ -1,6 +1,7 @@
 #ifndef __INCLUDE_GUARD_SWUPD_INTERNAL_H
 #define __INCLUDE_GUARD_SWUPD_INTERNAL_H
 
+extern int autoupdate_main(int argc, char **argv);
 extern int bundle_add_main(int argc, char **argv);
 extern int bundle_remove_main(int argc, char **argv);
 extern int bundle_list_main(int argc, char **argv);

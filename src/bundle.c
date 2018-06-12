b4e64178be214756cce90dd7321d6ce41aa62752
Otavio Pontes <otavio.pontes@intel.com>
2018-06-12 20:19:52 +0000

    bundle-add: Use download resume on bundle-add too
    There's no reason to support pack resume on bundle-add

--- src/bundle.c
+++ src/bundle.c
@@ -728,7 +728,7 @@ static int install_bundles(struct list *bundles, struct list **subs, int current
 	(void)rm_staging_dir_contents("download");
 
 download_subscribed_packs:
-	if (list_longer_than(to_install_files, 10) && download_subscribed_packs(*subs, mom, true, false)) {
+	if (list_longer_than(to_install_files, 10) && download_subscribed_packs(*subs, mom, true, true)) {
 		if (retries < MAX_TRIES && !content_url_is_local) {
 			increment_retries(&retries, &timeout);
 			printf("\nRetry #%d downloading subscribed packs\n", retries);

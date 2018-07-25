a15a60444023cfd4e2f00022594f72851a676193
Brian J Lovin <brian.j.lovin@intel.com>
2018-07-25 20:53:13 +0000

    Add fs.c - storage and size functions
    Introduce two new functions to make it easier to calculate the
    contentsize of a list of manifests, and to calculate the free
    space on a device.
    
    Eventually, move all fs related functions to this file.
    (ex mkdir_p, is_populated_dir, copy_all, etc)
    
    Signed-off-by: Brian J Lovin <brian.j.lovin@intel.com>

---
+++ src/fs.c
@@ -0,0 +1,47 @@
+/*
+ *   Software Updater - client side
+ *
+ *      Copyright (c) 2012-2018 Intel Corporation.
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, version 2 or later of the License.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "swupd.h"
+#include <sys/statvfs.h>
+
+long get_available_space(const char *path)
+{
+	struct statvfs stat;
+
+	if (statvfs(path, &stat) != 0) {
+		return -1;
+	}
+
+	return stat.f_bsize * stat.f_bavail;
+}
+
+long get_manifest_list_contentsize(struct list *manifests)
+{
+	long total_size = 0;
+
+	struct list *ptr = NULL;
+	for (ptr = list_head(manifests); ptr; ptr = ptr->next) {
+		struct manifest *subman;
+		subman = ptr->data;
+
+		total_size += subman->contentsize;
+	}
+
+	return total_size;
+}

721e4e468ee3dd759dd88abac2035f796091bd3e
Otavio Pontes <otavio.pontes@intel.com>
2018-09-11 10:00:44 -0700

    tp: Add an initial implementation of a Thread Pool
    Simple implementation of a thread pool to be used initially for parallel
    downloads. Only major features needed were implemented. A tp_wait() function is to be
    contributed later.
    
    Signed-off-by: Otavio Pontes <otavio.pontes@intel.com>
    Signed-off-by: Brian J Lovin <brian.j.lovin@intel.com>

---
+++ src/lib/thread_pool.h
@@ -0,0 +1,42 @@
+#ifndef __THREAD_POOL__
+#define __THREAD_POOL__
+
+#include <stdlib.h>
+
+/*
+ * Really simple and small thread pool implementation for swupd.
+ */
+
+struct tp;
+
+/*
+ * Thread task function type definition.
+ */
+typedef void (*tp_task_run_t)(void *data);
+
+/*
+ * Create a new thread pool.
+ *
+ * num_threads: The number of threads to be created.
+ *
+ * Note: Free thread pool data with tp_free()
+ */
+struct tp *tp_start(int num_threads);
+
+/*
+ * Schedule a task to be run in this thread pool.
+ *
+ * run: Callback to be executed in a new thread.
+ * data: Data informed to callback.
+ */
+int tp_task_schedule(struct tp *tp, tp_task_run_t run, void *data);
+
+/*
+ * Wait for all scheduled tasks to be completed, finishes all threads and
+ * release all memory used by the thread pool.
+ */
+void tp_complete(struct tp *tp);
+
+//TODO: Implement a tp_wait() function
+
+#endif

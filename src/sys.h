d106f7a47ecfa6c7cecfad19f45abfa1e889c980
Otavio Pontes <otavio.pontes@intel.com>
2018-11-15 23:20:04 +0000

    sys: Add run_command() function to replace some usages of system()
    System API is a very convenient way on executing external commands, but it has
    some problems for some usages. System always launches a shell to execute the
    desired command, so we can be affected by environment variables (like PATH) and
    shell globbing.
    
    We can avoid most of the environment variables affecting the swupd result by
    using the full path of the program being executed, but making sure the shell
    won't expand all wildcards is tricky and specially dangerous for some commands
    that we need to run, like cp, mkdir, etc.
    
    So to make sure no globbing will be performed we can use fork+execve to run
    commands instead of system.
    
    Signed-off-by: Otavio Pontes <otavio.pontes@intel.com>

--- src/sys.h
+++ src/sys.h
@@ -7,6 +7,31 @@ extern "C" {
 
 extern long get_available_space(const char *path);
 
+/* run_command_quiet: runs a command redirecting the standard and error outputs
+ * redirected to /dev/null. */
+#define run_command_quiet(...) run_command_full("/dev/null", "/dev/null", __VA_ARGS__)
+
+/* run_command: runs a command with standard and error output (stdout and stderr) set */
+#define run_command(...) run_command_full(NULL, NULL, __VA_ARGS__)
+
+/* run_command_full: Run command cmd with parameters informed in a NULL
+ * terminated list of strings.
+ *
+ * Return a negative number (-errno) if we are unable to create a new thread to
+ * run the command and a positive exit code (0-255) if the command was executed.
+ * If there's an error in the execution of the program 255 may also be returned
+ *
+ * Notes:
+ * - This function doesn't execute on a shell like system() so we won't have any
+ *   issue with parameters globbing.
+ * - The full path to cmd is required.
+ *
+ * Example:
+ * run_command(NULL, NULL, "/usr/bin/ls", "/tmp", NULL);
+ * This will run "ls /tmp" and redirect output to stdout and stderr.
+ */
+int run_command_full(const char *stdout_file, const char *stderr_file, const char *cmd, ...);
+
 #ifdef __cplusplus
 }
 #endif

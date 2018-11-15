91a4126ba0a99c8230a35bcf8a73e30df286a703
Otavio Pontes <otavio.pontes@intel.com>
2018-11-15 23:20:04 +0000

    sys: Add run_command() function to replace some usages of system()
    System API is a very convenient way on executing external commands, but it has
    some problems for some usages. System always launches a shell to execute the
    desired command, so we can be affected by environment variables (like PATH) and
    shell globbing.
    
    We can avoid most of the environment variables affecting the swupd result by
    using the full path of the program being executed, but making sure the shell
    won't expand all wildcards is tricky and specially dangerous for some commands
    that we need to run, like cp, mkdir, etc.
    
    So to make sure no globbing will be performed we can use fork+execve to run
    commands instead of system.
    
    Signed-off-by: Otavio Pontes <otavio.pontes@intel.com>

--- src/sys.c
+++ src/sys.c
@@ -18,7 +18,114 @@
  */
 
 #include "sys.h"
+
+#include <errno.h>
+#include <fcntl.h>
+#include <libgen.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/statvfs.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+static int replace_fd(int fd, const char *fd_file)
+{
+	int new_fd;
+
+	if (!fd_file) {
+		return 0;
+	}
+	new_fd = open(fd_file, O_WRONLY | O_CREAT, S_IRWXU);
+	if (new_fd < 0) {
+		return new_fd;
+	}
+
+	return dup2(new_fd, fd);
+}
+
+int run_command_full(const char *stdout_file, const char *stderr_file, const char *cmd, ...)
+{
+	int pid, ret, child_ret;
+
+	pid = fork();
+	if (pid < 0) {
+		fprintf(stderr, "run_command %s failed: %i (%s)\n", cmd, errno, strerror(errno));
+		return -errno;
+	}
+
+	// Main
+	if (pid > 0) {
+		while ((ret = waitpid(pid, &child_ret, 0)) < 0) {
+			if (errno != EINTR) {
+				fprintf(stderr, "run_command %s failed: %i (%s)\n",
+					cmd, errno, strerror(errno));
+				return -errno;
+			}
+		}
+
+		return WEXITSTATUS(child_ret);
+	}
+
+	// Child
+	va_list ap;
+	char *short_cmd;
+	char **params;
+	int args_count = 0, i = 0;
+	char *arg;
+
+	va_start(ap, cmd);
+	while ((arg = va_arg(ap, char *)) != NULL) {
+		args_count++;
+	}
+	va_end(ap);
+
+	// Create params array with space for all parameters,
+	// command basename and NULL terminator
+	params = malloc(sizeof(char *) * (args_count + 2));
+	if (!params) {
+		goto error_child;
+	}
+
+	// We need a copy because of basename
+	short_cmd = strdup(cmd);
+	if (!short_cmd) {
+		abort();
+	}
+
+	params[i++] = basename(short_cmd);
+
+	va_start(ap, cmd);
+	while ((arg = va_arg(ap, char *)) != NULL) {
+		params[i++] = arg;
+	}
+	va_end(ap);
+
+	params[i] = NULL;
+
+	if (replace_fd(STDOUT_FILENO, stdout_file) < 0) {
+		goto error_child;
+	}
+
+	if (replace_fd(STDERR_FILENO, stderr_file) < 0) {
+		goto error_child;
+	}
+
+	ret = execve(cmd, params, NULL);
+	if (ret < 0) {
+		fprintf(stderr, "run_command %s failed: %i (%s)\n", cmd, errno, strerror(errno));
+	}
+	free(params);
+	free(short_cmd);
+
+	exit(EXIT_FAILURE); // execve nevers return on success
+	return 0;
+
+error_child:
+	exit(255);
+	return 0;
+}
 
 long get_available_space(const char *path)
 {

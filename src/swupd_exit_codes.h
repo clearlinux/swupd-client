ff18daa68014b465f90586037c63ffe9a646e3ce
Castulo Martinez <castulo.martinez@intel.com>
2019-01-22 22:05:42 +0000

    clean should return consistent exit codes
    Regardless of what codes are used internally, swupd should always
    exit with a code defined in swupd_exit_codes.h.
    
    This commit makes those changes for clean.
    
    Signed-off-by: Castulo Martinez <castulo.martinez@intel.com>

--- src/swupd_exit_codes.h
+++ src/swupd_exit_codes.h
@@ -41,7 +41,8 @@ typedef enum {
 	SWUPD_COULDNT_RENAME_FILE,      /* 28 couldn't rename a file */
 	SWUPD_SUBPROCESS_ERROR,		/* 29 failure to execute another program in a subprocess */
 	SWUPD_COULDNT_LIST_DIR,		/* 30 couldn't list the content of a directory */
-	SWUPD_COMPUTE_HASH_ERROR	/* 31 there was an error computing the hash of the specified file */
+	SWUPD_COMPUTE_HASH_ERROR,       /* 31 there was an error computing the hash of the specified file */
+	SWUPD_COULDNT_GET_TIME		/* 32 couldn't get current system time */
 
 } swupd_code;
 

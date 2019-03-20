7d6fa1db051f47ddc886f493a5b6902730534603
Otavio Pontes <otavio.pontes@intel.com>
2019-03-20 16:22:22 -0700

    search: Add support to regular expressions in search-file
    Signed-off-by: Otavio Pontes <otavio.pontes@intel.com>

--- swupd.bash
+++ swupd.bash
@@ -57,7 +57,7 @@ _swupd()
 		opts="--help"
 		break;;
 	    ("search-file")
-		opts="--help --library --binary --top --csv --init --ignore-time --url --contenturl --versionurl --port --path --format --statedir --certpath -debug --quiet "
+		opts="--help --library --binary --top --csv --init --ignore-time --url --contenturl --versionurl --port --path --format --statedir --certpath --regexp --debug --quiet "
 		break;;
 	    ("info")
 		opts="--debug --quiet "

6d2ab6a20cf8c634115cc28b9f1a3acf8d8c24ea
Otavio Pontes <otavio.pontes@intel.com>
2019-04-16 13:27:10 -0700

    signature: Minor changes in style to make code style consistent
     - Renaming functions
     - improvements in comment headers
     - Minor .h style changes
    
    Signed-off-by: Otavio Pontes <otavio.pontes@intel.com>

--- src/signature.h
+++ src/signature.h
@@ -1,26 +1,38 @@
-#ifndef SIGNATURE_H_
-#define SIGNATURE_H_
+#ifndef __SIGNATURE_H__
+#define __SIGNATURE_H__
 
 #include <stdbool.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /*
  * Initialize this module.
- * @return true <=> no error
+ *
+ * @return true if no error
  */
-bool initialize_signature(void);
+bool signature_init(void);
 
 /*
  * Terminate usage of this module, free resources.
  */
-void terminate_signature(void);
+void signature_deinit(void);
 
 /*
  * The given data file has already been downloaded from the given URL.
  * Download the corresponding signature file, and verify the data against the signature.
  * Delete the signature file if and only if the verification fails.
- * @param data_url - the URL from which the data came
- * @param data_filename - the file containing the data
+ *
+ * @param data_url the URL from which the data came
+ * @param data_filename the file containing the data
+ *
+ * @return true if no error
  */
-bool download_and_verify_signature(const char *data_url, const char *data_filename, int version, bool mix_exists);
+bool signature_verify(const char *data_url, const char *data_filename, int version, bool mix_exists);
 
-#endif /* SIGNATURE_H_ */
+#ifdef __cplusplus
+}
+#endif
+
+#endif

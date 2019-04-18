a3a58b7319ff5cf581e17947182db2d829023389
Otavio Pontes <otavio.pontes@intel.com>
2019-04-18 12:02:05 -0700

    test: Adding extra tests for signature
    Test if certificates with Authority Information Access property set as critical
    aren't used to validate swupd content.
    
    Signed-off-by: Otavio Pontes <otavio.pontes@intel.com>

--- test/unit/test_signature.c
+++ test/unit/test_signature.c
@@ -57,6 +57,9 @@ static int test_sign(const char *private_key, const char *public_key, bool succe
 	}
 
 	if (!signature_init(public_key, NULL)) {
+		if (!success) { // Error expected
+			ret = 0;
+		}
 		goto error;
 	}
 
@@ -76,33 +79,105 @@ error:
 
 static int test_signature()
 {
-	char *private_key, *public_key;
-	int ret;
+	char *private_key, *public_key, *dir, *config_ocsp,
+	     *config_ocsp_critical, *config_noocsp_critical;
+	int result = -1, ret;
 
-	private_key = str_or_die("%s/private.pem", test_dir);
-	public_key = str_or_die("%s/public.pem", test_dir);
+	private_key = sys_path_join(test_dir, "private.pem");
+	public_key = sys_path_join(test_dir, "public.pem");
+	dir = sys_dirname(__FILE__);
+	config_ocsp = sys_path_join(dir, "data/config_ocsp.cnf");
+	config_noocsp_critical = sys_path_join(dir, "data/config_noocsp_critical.cnf");
+	config_ocsp_critical = sys_path_join(dir, "data/config_ocsp_critical.cnf");
 
 	// Create a good key
+	printf("Testing with a valid RSA certificate\n");
+	rm_rf(private_key);
+	rm_rf(public_key);
 	ret = run_command("/usr/bin/openssl", "req", "-x509", "-sha256",
 			  "-nodes", "-days", "365", "-newkey", "rsa:4096",
 			  "-keyout", private_key, "-out", public_key,
 			  "-subj", "/C=US/ST=Oregon/L=Portland/O=Company Name/OU=Org/CN=localhost", NULL);
 	if (ret != 0) {
-		return -1;
+		goto error;
 	}
 
 	ret = test_sign(private_key, public_key, true);
+	if (ret != 0) {
+		goto error;
+	}
 
-	//TODO: Add tests with invalid signatures - expired, OCSP
+	// OCSP not critical - Should work
+	printf("Testing with a certificate with OCSP support\n");
+	rm_rf(private_key);
+	rm_rf(public_key);
+	ret = run_command("/usr/bin/openssl", "req", "-x509", "-sha256",
+			  "-nodes", "-days", "365", "-newkey", "rsa:4096",
+			  "-keyout", private_key, "-out", public_key,
+			  "-config", config_ocsp,
+			  "-subj", "/C=US/ST=Oregon/L=Portland/O=Company Name/OU=Org/CN=localhost", NULL);
+	if (ret != 0) {
+		goto error;
+	}
 
+	ret = test_sign(private_key, public_key, true);
+	if (ret != 0) {
+		goto error;
+	}
+
+	// OCSP not critical - Should fail
+	printf("Testing with a certificate with with OCSP support\n");
+	rm_rf(private_key);
+	rm_rf(public_key);
+	ret = run_command("/usr/bin/openssl", "req", "-x509", "-sha256",
+			  "-nodes", "-days", "365", "-newkey", "rsa:4096",
+			  "-keyout", private_key, "-out", public_key,
+			  "-config", config_ocsp_critical,
+			  "-subj", "/C=US/ST=Oregon/L=Portland/O=Company Name/OU=Org/CN=localhost", NULL);
+	if (ret != 0) {
+		goto error;
+	}
+
+	ret = test_sign(private_key, public_key, false);
+	if (ret != 0) {
+		goto error;
+	}
+
+	// NO OCSP critical - Should fail
+	printf("Testing with a certificate without OCSP support, but critical\n");
+	rm_rf(private_key);
+	rm_rf(public_key);
+	ret = run_command("/usr/bin/openssl", "req", "-x509", "-sha256",
+			  "-nodes", "-days", "365", "-newkey", "rsa:4096",
+			  "-keyout", private_key, "-out", public_key,
+			  "-config", config_noocsp_critical,
+			  "-subj", "/C=US/ST=Oregon/L=Portland/O=Company Name/OU=Org/CN=localhost", NULL);
+	if (ret != 0) {
+		goto error;
+	}
+
+	ret = test_sign(private_key, public_key, false);
+	if (ret != 0) {
+		goto error;
+	}
+
+
+	result = 0;
+error:
+	// Release resources
 	free(private_key);
 	free(public_key);
+	free(dir);
+	free(config_ocsp);
+	free(config_ocsp_critical);
+	free(config_noocsp_critical);
 
-	return ret;
+	return result;
 }
 
 int main()
 {
+	int result = EXIT_FAILURE;
 
 	if (set_up() < 0) {
 		print_error("Test set_up() error");
@@ -113,10 +188,9 @@ int main()
 		goto error;
 	}
 
-	tear_down();
 
-	return 0;
-
+	result = 0;
 error:
-	return EXIT_FAILURE;
+	tear_down();
+	return result;
 }

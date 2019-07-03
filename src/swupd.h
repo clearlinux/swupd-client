d0ef625366402bedd8b645fcf4bcf973c62b81c0
Otavio Pontes <otavio.pontes@intel.com>
2019-07-03 15:32:37 -0700

    bundle-add: Remove short option from skip-optional-bundles
    Trying to keep the number of short options flags low to be used only for more common commands
    
    Signed-off-by: Otavio Pontes <otavio.pontes@intel.com>

--- src/swupd.h
+++ src/swupd.h
@@ -186,7 +186,7 @@ extern char **swupd_argv;
 extern char *bundle_to_add;
 extern char *state_dir;
 extern int skip_diskspace_check;
-extern bool skip_optional_bundles;
+extern int skip_optional_bundles;
 extern timelist *global_times;
 extern int max_retries;
 extern int retry_delay;

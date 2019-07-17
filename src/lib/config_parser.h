4cb70835baf71e0843c8cbc97967d1e3c8ec614d
Otavio Pontes <otavio.pontes@intel.com>
2019-07-16 21:46:55 -0700

    global: Don't depend on a static variable to store context values
    Add a data parameter to config parser to make the config_parse function easier to
    be reused for other cases. And adapting config_loader to use that new function.
    
    Signed-off-by: Otavio Pontes <otavio.pontes@intel.com>

--- src/lib/config_parser.h
+++ src/lib/config_parser.h
@@ -16,19 +16,20 @@ extern "C" {
 /**
  * @brief Function type to process the parsed key/values
  */
-typedef bool (*load_config_fn_t)(char *section, char *key, char *value);
+typedef bool (*parse_config_fn_t)(char *section, char *key, char *value, void *data);
 
 /**
  * @brief Parses the INI style configuration file and sets up the runtime environment
  * based on the configuration values
  * @param filename: the full path to the configuration file to parse
- * @param load_config_fn: a pointer to a function that takes specific actions based
+ * @param parse_config_fn: a pointer to a function that takes specific actions based
  * on the key/value pairs
+ * @param data: user's data to be used on parse_config_fn() calls.
  *
  * @returns True if there were no errors when parsing and processing the parsed options,
  *          False otherwise
  */
-bool config_parse(const char *filename, load_config_fn_t load_config_fn);
+bool config_parse(const char *filename, parse_config_fn_t parse_config_fn, void *data);
 
 #ifdef __cplusplus
 }

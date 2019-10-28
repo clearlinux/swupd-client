a0aff2c3eb878000e03828158292639b0266fa31
Otavio Pontes <otavio.pontes@intel.com>
2019-10-28 15:21:52 -0700

    swupd_internal: Follow style from other headers in swupd_internal.h
    Signed-off-by: Otavio Pontes <otavio.pontes@intel.com>

--- src/swupd_internal.h
+++ src/swupd_internal.h
@@ -1,23 +1,30 @@
-#ifndef __INCLUDE_GUARD_SWUPD_INTERNAL_H
-#define __INCLUDE_GUARD_SWUPD_INTERNAL_H
+#ifndef __SWUPD_INTERNAL__
+#define __SWUPD_INTERNAL__
 
-extern enum swupd_code autoupdate_main(int argc, char **argv);
-extern enum swupd_code bundle_add_main(int argc, char **argv);
-extern enum swupd_code bundle_remove_main(int argc, char **argv);
-extern enum swupd_code bundle_list_main(int argc, char **argv);
-extern enum swupd_code hashdump_main(int argc, char **argv);
-extern enum swupd_code update_main(int argc, char **argv);
-extern enum swupd_code check_update_main(int argc, char **argv);
-extern enum swupd_code search_file_main(int argc, char **argv);
-extern enum swupd_code info_main(int argc, char **argv);
-extern enum swupd_code clean_main(int argc, char **argv);
-extern enum swupd_code mirror_main(int argc, char **argv);
-extern enum swupd_code binary_loader_main(int argc, char **argv);
-extern enum swupd_code install_main(int argc, char **argv);
-extern enum swupd_code repair_main(int argc, char **argv);
-extern enum swupd_code diagnose_main(int argc, char **argv);
-extern enum swupd_code bundle_info_main(int argc, char **argv);
-extern enum swupd_code verify_main(int argc, char **argv);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+enum swupd_code autoupdate_main(int argc, char **argv);
+enum swupd_code bundle_add_main(int argc, char **argv);
+enum swupd_code bundle_remove_main(int argc, char **argv);
+enum swupd_code bundle_list_main(int argc, char **argv);
+enum swupd_code hashdump_main(int argc, char **argv);
+enum swupd_code update_main(int argc, char **argv);
+enum swupd_code check_update_main(int argc, char **argv);
+enum swupd_code search_file_main(int argc, char **argv);
+enum swupd_code info_main(int argc, char **argv);
+enum swupd_code clean_main(int argc, char **argv);
+enum swupd_code mirror_main(int argc, char **argv);
+enum swupd_code binary_loader_main(int argc, char **argv);
+enum swupd_code install_main(int argc, char **argv);
+enum swupd_code repair_main(int argc, char **argv);
+enum swupd_code diagnose_main(int argc, char **argv);
+enum swupd_code bundle_info_main(int argc, char **argv);
+enum swupd_code verify_main(int argc, char **argv);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif

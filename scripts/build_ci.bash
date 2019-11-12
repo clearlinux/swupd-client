d17e818e54bf35041f432c5a82511443eded15aa
Otavio Pontes <otavio.pontes@intel.com>
2019-11-12 13:41:46 -0800

    actions: Build dependencies in parallel to have a faster build
    Download and build depedencies in parallel to try to maximize usage of CPU,
    by building things at the same time we run downloads
    
    Signed-off-by: Otavio Pontes <otavio.pontes@intel.com>

--- scripts/build_ci.bash
+++ scripts/build_ci.bash
@@ -1,40 +1,59 @@
 #!/bin/bash
 set -e
-CORES=2
+CORES=1
 
-# Install dependencies from ubuntu
-sudo apt-get install python3-docutils
-sudo apt-get install shellcheck
-sudo apt-get install doxygen
-sudo apt-get install check
-sudo apt-get install bats
-sudo pip install coverxygen
-sudo ln -s /bin/systemctl /usr/bin/systemctl
+apt_get_update() {
+	# Install dependencies from ubuntu
+	sudo apt-get install python3-docutils
+	sudo apt-get install shellcheck
+	sudo apt-get install doxygen
+	sudo apt-get install check
+	sudo apt-get install bats
+	sudo pip install coverxygen
 
-# build bsdiff
-wget https://github.com/clearlinux/bsdiff/releases/download/v1.0.2/bsdiff-1.0.2.tar.xz
-tar -xvf bsdiff-1.0.2.tar.xz
-pushd bsdiff-1.0.2 && ./configure --prefix=/usr --disable-tests && make -j$CORES && sudo make install && popd
+	# set expected PATH for systemctl
+	sudo ln -s /bin/systemctl /usr/bin/systemctl
+	# Use rst2man from python3
+	sudo ln -s /usr/share/docutils/scripts/python3/rst2man /usr/bin/rst2man.py
+}
 
-## build curl
-wget https://curl.haxx.se/download/curl-7.64.0.tar.gz
-tar -xvf curl-7.64.0.tar.gz
-pushd curl-7.64.0 && ./configure --prefix=/usr --libdir=/usr/lib/x86_64-linux-gnu && make -j$CORES && sudo make install && popd
+build_bsdiff() {
+	# build bsdiff
+	wget https://github.com/clearlinux/bsdiff/releases/download/v1.0.2/bsdiff-1.0.2.tar.xz
+	tar -xvf bsdiff-1.0.2.tar.xz
+	pushd bsdiff-1.0.2 && ./configure --prefix=/usr --disable-tests && make -j$CORES && sudo make install && popd
+}
 
-# build libarchive
-wget https://github.com/libarchive/libarchive/archive/v3.3.1.tar.gz
-tar -xvf v3.3.1.tar.gz
-pushd libarchive-3.3.1 && autoreconf -fi && ./configure --prefix=/usr && make -j$CORES && sudo make install && popd
+build_curl() {
+	## build curl
+	wget https://curl.haxx.se/download/curl-7.64.0.tar.gz
+	tar -xvf curl-7.64.0.tar.gz
+	pushd curl-7.64.0 && ./configure --prefix=/usr --libdir=/usr/lib/x86_64-linux-gnu && make -j$CORES && sudo make install && popd
+}
 
-# Use rst2man from python3
-sudo ln -s /usr/share/docutils/scripts/python3/rst2man /usr/bin/rst2man.py
+build_libarchive() {
+	# build libarchive
+	wget https://github.com/libarchive/libarchive/archive/v3.3.1.tar.gz
+	tar -xvf v3.3.1.tar.gz
+	pushd libarchive-3.3.1 && autoreconf -fi && ./configure --prefix=/usr && make -j$CORES && sudo make install && popd
+}
 
-# Install new clang-format from llvm repository
-wget -O llvm.gpg.key https://apt.llvm.org/llvm-snapshot.gpg.key
-sudo apt-key add llvm.gpg.key
-sudo add-apt-repository "deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-9 main"
-sudo apt-get update
-sudo apt-get install -y clang-format-9
+install_clang() {
+	# Install new clang-format from llvm repository
+	wget -O llvm.gpg.key https://apt.llvm.org/llvm-snapshot.gpg.key
+	sudo apt-key add llvm.gpg.key
+	sudo add-apt-repository "deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-9 main"
+	sudo apt-get update
+	sudo apt-get install -y clang-format-9
+}
+
+apt_get_update &
+build_bsdiff &
+build_curl &
+build_libarchive &
+install_clang &
+
+wait
 
 # Build Swupd
 autoreconf --verbose --warnings=none --install --force

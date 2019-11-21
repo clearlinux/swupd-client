dd00a16659323c70ac137490ee53361043da127b
Castulo Martinez <castulo.martinez@intel.com>
2019-11-21 11:25:30 -0800

    Adding autocompletion for 3rd-party bundle-add
    Signed-off-by: Castulo Martinez <castulo.martinez@intel.com>

--- swupd.zsh
+++ swupd.zsh
@@ -190,6 +190,7 @@ if [[ -n "$state" ]]; then
           '(help)list[List third party repo(s)]'
           '(help)remove[Remove third party repo]'
           '(help)add[Add third party repo]'
+          '(help)bundle-add[Install a bundle from a third party repository]'
           _arguments $thirdparty && ret=0
           ;;
           add)

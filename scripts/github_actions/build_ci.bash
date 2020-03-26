eeeb839c4197ebf30eab151287593c8d5cd6fe97
Otavio Pontes <otavio.pontes@intel.com>
2020-03-26 15:06:44 -0700

    actions: cache dependencies for swupd
    Some packages we need to build swupd are not available on ubunutu-latest
    so we need to download and build from source. This process is slow and
    caching can save us some significant time on build.

--- scripts/github_actions/build_ci.bash
+++ scripts/github_actions/build_ci.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Install all dependencies available on ubuntu-latest from github actions to
+# build swupd and run all tests.
+# Builds swupd with configuration needed to run swupd tests
+
 set -e
 CORES=2
 
@@ -10,21 +14,6 @@ sudo ln -s /bin/systemctl /usr/bin/systemctl
 
 # Use rst2man from python3
 sudo ln -s /usr/share/docutils/scripts/python3/rst2man /usr/bin/rst2man.py
-
-# build bsdiff
-wget https://github.com/clearlinux/bsdiff/releases/download/v1.0.2/bsdiff-1.0.2.tar.xz
-tar -xvf bsdiff-1.0.2.tar.xz
-pushd bsdiff-1.0.2 && ./configure --prefix=/usr --disable-tests && make -j$CORES && sudo make install && popd
-
-## build curl
-wget https://curl.haxx.se/download/curl-7.64.0.tar.gz
-tar -xvf curl-7.64.0.tar.gz
-pushd curl-7.64.0 && ./configure --prefix=/usr --libdir=/usr/lib/x86_64-linux-gnu && make -j$CORES && sudo make install && popd
-
-# build libarchive
-wget https://github.com/libarchive/libarchive/archive/v3.3.1.tar.gz
-tar -xvf v3.3.1.tar.gz
-pushd libarchive-3.3.1 && autoreconf -fi && ./configure --prefix=/usr && make -j$CORES && sudo make install && popd
 
 # Build Swupd
 autoreconf --verbose --warnings=none --install --force

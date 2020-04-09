b703ff1a8ec3c800cf27c4d33ddab4e6c5c63b57
Otavio Pontes <otavio.pontes@intel.com>
2020-04-09 13:58:15 -0700

    Create the UNEXPECTED() macro
    There are several points in swupd that we find conditions that should never
    happen, but we try to correct them anyway to be more reliable. Because of that
    we may hide programming mistakes that are corrected later.
    
    So, to make sure we are going to catch those problems in debug mode in our development
    machines and test environment, I am suggesting a UNEXPECTED() macro that aborts swupd
    if --enable-debug was used to build swupd.
    
    I am adding some examples on what we could do with that.
    
    Signed-off-by: Otavio Pontes <otavio.pontes@intel.com>

--- src/lib/macros.h
+++ src/lib/macros.h
@@ -18,4 +18,14 @@
 /** @brief Return the max between 2 values. */
 #define MAX(_a, _b) ((_a) > (_b) ? (_a) : (_b))
 
+#ifdef DEBUG_MODE
+#define UNEXPECTED() abort()
+#else
+/**
+ * @brief To be used in all checks where we assume swupd should never get in to.
+ * This macro aborts execution only when DEBUG_MODE is used.
+ */
+#define UNEXPECTED()
+#endif
+
 #endif

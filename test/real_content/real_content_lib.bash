7820334bb849c1c0b8b89a12e1c4c66c00cd77ec
William Douglas <william.douglas@intel.com>
2023-02-16 10:20:41 -0800

    Update shellcheck exceptions
    Shellcheck changed its detection for a few file sourcing errors from
    1090 to 1091. Update the disables accordingly.
    
    Signed-off-by: William Douglas <william.douglas@intel.com>

--- test/real_content/real_content_lib.bash
+++ test/real_content/real_content_lib.bash
@@ -2,8 +2,8 @@
 
 #TODO: Add test for crossing a format barrier
 
-# shellcheck disable=SC1090
-# SC1090: Can't follow non-constant source. Use a directive to specify location.
+# shellcheck disable=SC1091
+# SC1091: Not following, SC couldn't follow the dynamic path
 # We already process that file, so it's fine to ignore
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_PATH/../functional/testlib.bash"
